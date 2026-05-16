5788751a73ff62f4db77b4adfdf9f21fd37c322b
AdriRP <adri03lol@gmail.com>
2026-05-16 13:56:45 +0100

    Remove commented-out code

--- steam-feeder/src/main/java/org/ulpgc/dacd/thecodeknights/model/SteamEvent.java
+++ steam-feeder/src/main/java/org/ulpgc/dacd/thecodeknights/model/SteamEvent.java
@@ -53,31 +53,3 @@ public class SteamEvent {
         return players != null;
     }
 }
-
-
-/*
-public class SteamEvent {
-    private final String ts;
-    private final String ss;
-
-    private final String appId;
-    private final String name;
-    private final Integer players;
-
-    public SteamEvent(String ts, String ss, String appId, String name, Integer players) {
-        this.ts = ts;
-        this.ss = ss;
-        this.appId = appId;
-        this.name = name;
-        this.players = players;
-    }
-
-    public String getTs() { return ts; }
-    public String getSs() { return ss; }
-    public String getAppId() { return appId; }
-    public String getName() { return name; }
-    public Integer getCurrentPlayers() { return players; }
-}
-
-
- */
